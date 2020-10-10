5abdea09f66fe30202927ef8193f2b0f6fd4f46e
Sky-Titan <er65119@naver.com>
2020-10-10 17:28:54 +0900

    구글맵 추가

--- app/src/main/java/org/techtown/moback/fragment/GraphFragment.java
+++ app/src/main/java/org/techtown/moback/fragment/GraphFragment.java
@@ -8,59 +8,61 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import org.techtown.moback.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GraphFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class GraphFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View view;
+    private GoogleMap mMap;
 
     public GraphFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GraphFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GraphFragment newInstance(String param1, String param2) {
-        GraphFragment fragment = new GraphFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false);
+        view = inflater.inflate(R.layout.fragment_graph, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+
+            LatLng SEOUL = new LatLng(37.56, 126.97);
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(SEOUL);
+            markerOptions.title("서울");
+            markerOptions.snippet("한국의 수도");
+            mMap.addMarker(markerOptions);
+
+
+            // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+
+            // CameraUpdateFactory.zoomTo가 오동작하네요.
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+            //mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+        });
+
+        return view;
     }
 }
