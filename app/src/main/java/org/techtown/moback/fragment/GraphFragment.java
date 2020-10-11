44d79264aee984f9e1339d9e39efa64aa0a5df80
Sky-Titan <er65119@naver.com>
2020-10-11 13:43:08 +0900

    make circle feature and change marker icon

--- app/src/main/java/org/techtown/moback/fragment/GraphFragment.java
+++ app/src/main/java/org/techtown/moback/fragment/GraphFragment.java
@@ -1,6 +1,8 @@
 package org.techtown.moback.fragment;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -17,12 +19,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -55,7 +61,13 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
 
     private ConstraintLayout bottom_info_layout;
     private ImageButton favorite_btn;
-    private Button getseat_btn;
+    private Button get_seat_btn;
+
+    private TextView store_name_tv;
+    private TextView store_address_tv;
+
+    private boolean isFavorite = false;
+    private int radius = 500;
 
     public GraphFragment() {
         // Required empty public constructor
@@ -77,7 +89,25 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
         previous_marker = new ArrayList<Marker>();
         bottom_info_layout = view.findViewById(R.id.storeinfo_layout);
         favorite_btn = view.findViewById(R.id.favorite_btn_graph);
-        getseat_btn = view.findViewById(R.id.getseat_btn_graph);
+        get_seat_btn = view.findViewById(R.id.getseat_btn_graph);
+
+        store_name_tv = view.findViewById(R.id.storename_tv);
+        store_address_tv = view.findViewById(R.id.store_address_tv);
+
+        //가게 좋아요 버튼
+        favorite_btn.setOnClickListener(view1 -> {
+            //TODO : 추후에 수정
+            if(!isFavorite)
+            {
+                isFavorite = true;
+                favorite_btn.setImageDrawable(getActivity().getDrawable(R.drawable.ic_baseline_favorite_24));
+            }
+            else
+            {
+                isFavorite = false;
+                favorite_btn.setImageDrawable(getActivity().getDrawable(R.drawable.ic_baseline_favorite_border_24));
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
@@ -86,53 +116,84 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
         //현재 위치
         myLocation_btn = view.findViewById(R.id.current_position_fab_graph);
         myLocation_btn.setOnClickListener(view1 -> {
-            Location location = getMyLocation();
-            if(location != null)
-            {
-                double lng = location.getLongitude();
-                double lat = location.getLatitude();
-
-                LatLng position = new LatLng(lat, lng);
-                addMarkerInMap(position, "현재 위치", getAddress(position));
-                moveCameraTo(position);
-                showPlaceInformation(position);
-            }
+            LatLng latLng = toLatLng(getMyLocation());
+            showPlaceInformation(latLng);
+            goToMyLocation();
         });
 
         return view;
     }
+
+    public boolean isVisibleInfoLayout()
+    {
+        if(bottom_info_layout.getVisibility() == View.VISIBLE)
+            return true;
+        return false;
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
 
+        //마커가 아닌곳 누를 땐 하단 레이아웃 숨김
         mMap.setOnMapClickListener(latLng -> {
             hideBottomInfoWindow();
-            getseat_btn.setBackground(getActivity().getDrawable(R.drawable.ripple_bg_gray));
         });
 
+        //정보레이아웃 보이기
         mMap.setOnMarkerClickListener(marker -> {
-            //TODO : 마커 클릭 시 반응
+            //마커 클릭 시 반응
             marker.showInfoWindow();
 
             //하단 정보 레이아웃
             showBottomInfoWindow();
-            getseat_btn.setBackground(getActivity().getDrawable(R.drawable.ripple_bg_primary));
+            setStoreInfo(marker.getTitle(), marker.getSnippet());
+            get_seat_btn.setBackground(getActivity().getDrawable(R.drawable.ripple_bg_primary));
             return true;
         });
 
+        LatLng latLng = toLatLng(getMyLocation());
+        showPlaceInformation(latLng);
+
+        goToMyLocation();
+    }
+
+    private void goToMyLocation()
+    {
         Location location = getMyLocation();
         if(location != null)
         {
+            LatLng position = toLatLng(location);
+
+            CircleOptions circleOptions = new CircleOptions().center(position)
+                    .radius(radius)
+                    .strokeWidth(0f)
+                    .fillColor(Color.parseColor("#4c6fea8a"));
+
+            addMarkerInMap(position, "현재 위치" ,getAddress(position), R.drawable.now_i_am, circleOptions);
+            moveCameraTo(position);
+
+        }
+    }
+
+    private LatLng toLatLng(Location location)
+    {
+        if(location != null) {
             double lng = location.getLongitude();
             double lat = location.getLatitude();
 
             LatLng position = new LatLng(lat, lng);
-            addMarkerInMap(position, "현재 위치" ,getAddress(position));
-            moveCameraTo(position);
-            showPlaceInformation(position);
+            return position;
         }
+        return null;
+    }
+
+    private void setStoreInfo(String store_name, String store_address)
+    {
+        store_name_tv.setText(store_name);
+        store_address_tv.setText(store_address);
     }
 
     private void showBottomInfoWindow()
@@ -140,8 +201,10 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
         bottom_info_layout.setVisibility(View.VISIBLE);
     }
 
-    private void hideBottomInfoWindow()
+    public void hideBottomInfoWindow()
     {
+        favorite_btn.setImageDrawable(getActivity().getDrawable(R.drawable.ic_baseline_favorite_border_24));
+        get_seat_btn.setBackground(getActivity().getDrawable(R.drawable.ripple_bg_gray));
         bottom_info_layout.setVisibility(View.GONE);
     }
 
@@ -173,13 +236,26 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
         return null;
     }
 
-    private Marker addMarkerInMap(LatLng position, String title, String snipet)
+    private Marker addMarkerInMap(LatLng position, String title, String snipet, int icon_id)
     {
         //내 위치에 마커표시
         MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(icon_id));
         markerOptions.position(position);
         markerOptions.title(title);
         markerOptions.snippet(snipet);
+        return mMap.addMarker(markerOptions);
+    }
+
+    private Marker addMarkerInMap(LatLng position, String title, String snipet, int icon_id, CircleOptions circleOptions)
+    {
+        //내 위치에 마커표시
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(icon_id));
+        markerOptions.position(position);
+        markerOptions.title(title);
+        markerOptions.snippet(snipet);
+        mMap.addCircle(circleOptions);
         return mMap.addMarker(markerOptions);
     }
 
@@ -220,9 +296,10 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
         }
     }
 
-    public void showPlaceInformation(LatLng location)
+    private void showPlaceInformation(LatLng location)
     {
         mMap.clear();//지도 클리어
+        goToMyLocation();
 
         if (previous_marker != null)
             previous_marker.clear();//지역정보 마커 클리어
@@ -231,7 +308,7 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
                 .listener(this)
                 .key("AIzaSyBIlMDJQLVxv6tNxqDyxAk5ErqPNm5Qmts")
                 .latlng(location.latitude, location.longitude)//현재 위치
-                .radius(500) //500 미터 내에서 검색
+                .radius(radius) //500 미터 내에서 검색
                 .type(PlaceType.CAFE) //카페
                 .build()
                 .execute();
@@ -250,12 +327,14 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
     @Override
     public void onPlacesSuccess(List<Place> places) {
         getActivity().runOnUiThread(() -> {
+
+
             for (noman.googleplaces.Place place : places) {
 
                 LatLng latLng = new LatLng(place.getLatitude(), place.getLongitude());
 
                 String markerSnippet = getAddress(latLng);
-                Marker item = addMarkerInMap(latLng, place.getName(), markerSnippet);
+                Marker item = addMarkerInMap(latLng, place.getName(), markerSnippet, R.drawable.cafe_w_24_8995_ff);
                 previous_marker.add(item);
             }
 
@@ -272,5 +351,7 @@ public class GraphFragment extends Fragment implements PlacesListener, OnMapRead
     public void onPlacesFinished() {
 
     }
+
+
 
 }
